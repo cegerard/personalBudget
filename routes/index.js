9fdfa52009e172b92a88ea9fe3db741c9d08972d
cegerard <git@eriatand.org>
2020-08-16 18:17:44 +0200

    add budget list page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const budgetList = require('../data/budgetList.json'); // TODO replace this by core service layer when available
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => res.redirect(`${req.baseUrl}/budgets`));
+
+router.get('/budgets', (req, res) => {
+  res.render('budgets', { page: 'budgets', budgetList });
+});
+
 });
 
 module.exports = router;
